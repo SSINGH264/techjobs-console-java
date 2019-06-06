417cb7bba301968e793b9dc0abc06429ec6f9d7a
SSINGH264 <samanta.singhd@gmail.com>
2019-06-05 21:47:20 -0500

    Added code for printJobs

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -111,6 +111,19 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No jobs found");
+        }
+
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println("*****");
+
+            for (String key : job.keySet()) {
+                System.out.println(key + ": " + job.get(key));
+            }
+
+            System.out.println("*****");
+        }
+
     }
 }
